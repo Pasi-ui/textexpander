0c1ba0b97c9ca1e2bc1e8cfb8287dcc5e0cef922
Pascal Zimmermann <pascalzimmermann@MacBookPro.fritz.box>
2026-04-20 22:49:15 +0200

    Stabile App ohne Crash - Persistenz funktioniert

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,6 @@
 use std::sync::{Arc, Mutex};
 use std::fs;
 use tauri::Manager;
-use std::thread;
-use enigo::KeyboardControllable;
 
 #[derive(Clone, serde::Serialize, serde::Deserialize)]
 struct Shortcut {
@@ -37,95 +35,6 @@ fn save_shortcuts(app: &tauri::AppHandle, shortcuts: &Vec<Shortcut>) {
     }
     let data = serde_json::to_string(shortcuts).unwrap();
     fs::write(file_path, data).unwrap();
-}
-
-fn start_keyboard_hook(state: Arc<AppState>) {
-    thread::spawn(move || {
-        use rdev::{listen, Event, EventType, Key};
-        
-        let mut buffer = String::new();
-        let mut enigo = enigo::Enigo::new();
-
-        let _ = listen(move |event: Event| {
-            match event.event_type {
-                EventType::KeyPress(key) => {
-                    // Convert key to character if possible
-                    let char_opt = match key {
-                        Key::Num0 => Some('0'),
-                        Key::Num1 => Some('1'),
-                        Key::Num2 => Some('2'),
-                        Key::Num3 => Some('3'),
-                        Key::Num4 => Some('4'),
-                        Key::Num5 => Some('5'),
-                        Key::Num6 => Some('6'),
-                        Key::Num7 => Some('7'),
-                        Key::Num8 => Some('8'),
-                        Key::Num9 => Some('9'),
-                        Key::KeyA => Some('a'),
-                        Key::KeyB => Some('b'),
-                        Key::KeyC => Some('c'),
-                        Key::KeyD => Some('d'),
-                        Key::KeyE => Some('e'),
-                        Key::KeyF => Some('f'),
-                        Key::KeyG => Some('g'),
-                        Key::KeyH => Some('h'),
-                        Key::KeyI => Some('i'),
-                        Key::KeyJ => Some('j'),
-                        Key::KeyK => Some('k'),
-                        Key::KeyL => Some('l'),
-                        Key::KeyM => Some('m'),
-                        Key::KeyN => Some('n'),
-                        Key::KeyO => Some('o'),
-                        Key::KeyP => Some('p'),
-                        Key::KeyQ => Some('q'),
-                        Key::KeyR => Some('r'),
-                        Key::KeyS => Some('s'),
-                        Key::KeyT => Some('t'),
-                        Key::KeyU => Some('u'),
-                        Key::KeyV => Some('v'),
-                        Key::KeyW => Some('w'),
-                        Key::KeyX => Some('x'),
-                        Key::KeyY => Some('y'),
-                        Key::KeyZ => Some('z'),
-                        _ => None,
-                    };
-
-                    if let Some(c) = char_opt {
-                        buffer.push(c);
-                        
-                        // Read current shortcuts from Mutex
-                        let shortcuts = state.shortcuts.lock().unwrap();
-                        
-                        // Check if any shortcut trigger matches the buffer
-                        for shortcut in shortcuts.iter() {
-                            if buffer.ends_with(&shortcut.trigger) {
-                                // Remove the trigger from buffer by pressing backspace
-                                for _ in 0..shortcut.trigger.len() {
-                                    enigo.key_down(enigo::Key::Backspace);
-                                    enigo.key_up(enigo::Key::Backspace);
-                                }
-                                
-                                // Type the expansion
-                                for c in shortcut.expansion.chars() {
-                                    enigo.key_click(enigo::Key::Layout(c));
-                                }
-                                
-                                // Clear buffer after match
-                                buffer.clear();
-                                break;
-                            }
-                        }
-                        
-                        // Limit buffer size to prevent excessive memory usage
-                        if buffer.len() > 100 {
-                            buffer.remove(0);
-                        }
-                    }
-                }
-                _ => {}
-            }
-        });
-    });
 }
 
 #[tauri::command]
@@ -163,11 +72,7 @@ pub fn run() {
             let state = Arc::new(AppState {
                 shortcuts: Mutex::new(shortcuts),
             });
-            app.manage(state.clone());
-            
-            // Start the keyboard hook thread
-            start_keyboard_hook(state);
-            
+            app.manage(state);
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
